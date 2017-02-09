ab428048fc441da3bd5ba4ef0476b2a2ef3d5546
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-09 09:48:02 -0600

    Cordelia question tracker toy.

---
+++ 014-cordelia.js
@@ -0,0 +1,9 @@
+// Find all <b> spans containing Cordelia
+	// Find those <blockquotes> that end with '?'
+// Find all <blockquotes> that end with '?'
+	// Find all following <b> spans including Lear
+	// Find all <blockquotes>
+
+cordelia = document.childNodes[1].childNodes[2].getElementsbyTagName("b").innerText = "Cordelia"
+
+cordLines = cordelia.parentElement.nextElementSibling.innerText
