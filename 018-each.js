f45ac61c1d83e41a1a887fc20f1beb167eb9662a
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-15 09:20:24 -0600

    Build 'each' in java.

---
+++ 018-each.js
@@ -0,0 +1,13 @@
+function each(array, x) {
+	for (var i = 0; i < array.length; i++){
+		var o = array[i];
+		return x(o);
+	}
+}
+
+names = ["Sumeet", "Beth", "Sonia", "Priya"];
+
+each(names, function(n){
+	console.log("Isn't " + n + " just great?");
+});
+
